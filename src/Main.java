bb14ffaeec434910caa16d5189a3dfbfc650fcc9
mearvk <mearvk@outlook.com>
2025-06-13 11:06:09 -0400

    Latest...

--- src/Main.java
+++ src/Main.java
@@ -60,6 +60,9 @@ public class Main
         interpreter.interpret("ADD COLUMN '//social security number' INTO TABLE '//employees'");
 
         interpreter.interpret("ADD COLUMN '//salary' INTO TABLE '//employees'");
+
+        interpreter.interpret("ADD COLUMN '//phone number' INTO TABLE '//employees'");
+
     }
 
 
