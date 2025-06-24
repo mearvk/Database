7bbed8d0792fd477ab8c93fd228e1a0770500f21
mearvk <mearvk@outlook.com>
2025-06-23 22:48:12 -0400

    Latest...

--- src/bodhi/client/BodhiClient.java
+++ src/bodhi/client/BodhiClient.java
@@ -1,13 +1,51 @@
 package bodhi.client;
 
+import java.net.Socket;
+import java.util.Scanner;
+
 public class BodhiClient
 {
-    public static class OutputListener
+    public static final String HOST = "mearvk.us";
+
+    public static final Integer PORT = 39001;
+
+    public Socket socket;
+
+    public Scanner scanner;
+
+    public BodhiClient()
+    {
+        try
+        {
+            this.socket = new Socket("mearvk.us",PORT);
+
+            this.scanner = new Scanner(System.in);
+
+            System.out.println("Connected to Remote Bodhi Server: [ "+BodhiClient.HOST+":"+BodhiClient.PORT+" ]");
+
+            String input = "";
+
+            while(input.equalsIgnoreCase("EXIT") || input.equalsIgnoreCase("EXIT;"))
+            {
+                input = scanner.next();
+
+
+            }
+        }
+        catch (Exception e)
+        {
+            return;
+        }
+
+
+    }
+
+    public static class OutputListener extends Thread
     {
 
     }
 
-    public static class InputListener
+    public static class InputListener extends Thread
     {
 
     }
